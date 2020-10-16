ea5699539e1ae87c4908373791904ef4a29b70cd
BogdanTelepov <mr.closer95@yandex.ru>
2020-10-16 18:13:06 +0600

    added api

--- app/src/main/java/ru/app/network/Api.kt
+++ app/src/main/java/ru/app/network/Api.kt
@@ -1,4 +1,12 @@
 package ru.app.network
 
+import kotlinx.coroutines.Deferred
+import retrofit2.Response
+import retrofit2.http.GET
+import ru.app.models.MovieResponse
+
 interface Api {
+
+    @GET("movie/popular")
+    suspend fun getPopularMovies(): Deferred<Response<MovieResponse>>
 }
